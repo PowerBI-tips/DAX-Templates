adfa7e6fd1a737abbaad2fa04a0cf77768a7547c
Mike Carlo <mike@powerbi.tips>
2021-11-11 22:41:49 -0600

    add: Directions to ReadmeGen.ps1

--- Utilities/PowerShell/ReadmeGen.ps1
+++ Utilities/PowerShell/ReadmeGen.ps1
@@ -6,9 +6,25 @@ Author: Kevin Arnold
 
 #>
 
+<# Instructions for using PowerShell ISE
+PowerShell ISE 
+https://docs.microsoft.com/en-us/powershell/scripting/windows-powershell/ise/introducing-the-windows-powershell-ise?view=powershell-7.2
+
+1. Change the directory to the root level of the Repo. For example the root directory where the DAX-Temmplates could be something like 'c:/repos/DAX-Templates'
+2. Open the ReadmeGen.ps1 file in the PowerShell ISE
+3. Run the ReadmeGen.ps1 by hitting F5 or clicking the Green Play button in the menu
+4. Script should run and update all the readme files in each of the Templates folder directories. 
+
+#> 
+
+# Start Location should be the location of the Root directory of the 
 $StartLocation = Get-Location
 $TemplateDir = $StartLocation.Path + '\templates'
 $TemplatesDirs = Get-ChildItem -Path $TemplateDir -Recurse | Where-Object {$_.PSIsContainer}
+
+Write-Output $StartLocation
+Write-Output $TemplateDir
+Write-Output $TemplatesDirs
 
 foreach ($d in $TemplatesDirs){
 
